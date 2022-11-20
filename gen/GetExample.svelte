02bd8971f00aa61254b67ac250df6919f76ddb5e
Liam Maguire <liammaguiresoftware@outlook.com>
2022-11-20 12:23:01 +1100

    Esto.

--- gen/GetExample.svelte
+++ gen/GetExample.svelte
@@ -2,6 +2,8 @@
   // Goal is to have it work with https://google.aip.dev/131
 
   // todo consider doing this via protogen import
+  import { onMount } from "svelte";
+  
   import {
     createConnectTransport,
     createPromiseClient,
@@ -9,7 +11,7 @@
 
   import {
     ExampleService
-  } from "../../gen/example_connectweb" // todo have the path determind by @ or from import
+  } from "../../gen/example_connectweb" // todo have the path determind by @ or from import (or just have a ts/js file imported to this script)
   
   export let name;
 
@@ -23,6 +25,12 @@
   
   let loading = true
   let res;
+  
+  // call via onMount
+  onMount(async () => {
+    return await getExample()
+  })
+
   async function getExample() {
     res = await client.getExample({name: name}) // todo pass in required fields
     loading = false
@@ -30,7 +38,7 @@
   // todo probably handle this nicer
   </script>
   
-  {#if res}
+  {#if res != null}
   <p>{res.name}</p><p>{res.display_name}</p>
   {/if}
   
