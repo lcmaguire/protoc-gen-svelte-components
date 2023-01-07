2df248661458afdfd11420c675725f48a4a4f750
Liam Maguire <liammaguiresoftware@outlook.com>
2023-01-07 15:30:59 -0300

    Update

--- plugin/protoc-gen-svelte-compenents.ts
+++ plugin/protoc-gen-svelte-compenents.ts
@@ -256,6 +256,65 @@ function genCreate(schema: Schema, method: DescMethod) {
   f.print(svelteTplate)
 }
 
+function genUpdate(schema: Schema, method: DescMethod) {
+  const f = schema.generateFile(`${method.name}.svelte`);
+
+  // https://github.com/bufbuild/protobuf-es/blob/main/docs/writing_plugins.md#message-options find message option 
+  // const option = findCustomMessageOption(method, 50007, ServiceOptions)
+  let te = findCustomMessageOption(method, 50007, MyMethodDesc)
+
+  let html = ""
+  html = htmlFromMessage(html, method.input)
+
+  let methodName = method.name 
+  const svelteTplate = `<script>
+  // Goal is to have it work with https://google.aip.dev/134
+  import {client} from "./client"
+  export let name;
+  import { onMount } from "svelte";
+
+
+  let loading = true
+  let res;
+
+  // method desc CreateineFoo
+
+  let msg = {};
+
+  onMount(async () => {
+      return await getExample() // TODO maybe this should be handled outside of this func (msg passed in.)
+  })
+
+  // todo have this func be imported
+  async function getExample() {
+      msg = await client.getExample({name: name}) // todo pass in required fields
+      loading = false
+  }
+
+  async function updateExample() {
+    // will need to build request to pass in.
+    // let updateRequest = {} // todo have this be message + fieldMask for updateRequst.
+    msg = await client.${formatMethodName(methodName)}(msg) // todo pass in required fields
+    loading = false
+    // should probably refresh page
+  }
+  // todo probably handle this nicer
+  </script>
+
+
+
+  <h3>${methodName}</h3>
+
+  ${html}
+
+  <button on:click={updateExample}>
+	  Make Update request
+  </button>
+  `
+
+  f.print(svelteTplate)
+}
+
 // todo consider making this recursive with current fieldPath being used for nested messages. eg req.Example -> 
 function htmlFromMessage(input: string, mess : DescMessage) {
   for (let i = 0; i < mess.fields.length; i++) {
