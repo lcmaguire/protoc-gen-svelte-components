b379d2d3b61202d2647d1a831f8343a733e885e2
Liam Maguire <liammaguiresoftware@outlook.com>
2023-01-08 13:52:39 -0300

    Regen.

--- gen/client.ts
+++ gen/client.ts
@@ -1,13 +1,13 @@
 
-  import {
-    createConnectTransport,
-    createPromiseClient,
-  } from "@bufbuild/connect-web";
-  import {ExampleService} from "./example_connectweb" // todo have this be done based upon out path + serviceName
+import {
+  createConnectTransport,
+  createPromiseClient,
+} from "@bufbuild/connect-web";
+import {ExampleService} from "./example_connectweb" // todo have this be done based upon out path + serviceName
 
 const transport = createConnectTransport({
-    baseUrl: "http://localhost:8080", // this should be set via config 
-  })
+  baseUrl: "http://localhost:8080", // this should be set via config 
+})
 const client = createPromiseClient(ExampleService, transport)
   
 export {client}
